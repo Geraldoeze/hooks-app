277eb3fd1601123323802e147c08f87ff5e00a5d
Geraldoeze <egerald344@gmail.com>
2022-01-13 20:08:48 +0100

    added firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,9 +7,25 @@ import Search from './Search';
 function Ingredients() {
   const [ userIngedients, setUserIngredients] = useState([]);
 
-  // useEffect();
+  useEffect(() => {
+    fetch('https://react-hooks-f4580-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for ( const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      })
+  }, []);
 
-
+  const filteredIngreHandler = filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }
   
   const addIngredientHandler = (ingredient) => {
     fetch('https://react-hooks-f4580-default-rtdb.firebaseio.com/ingredients.json', {
@@ -36,7 +52,7 @@ function Ingredients() {
       <IngredientForm onAdd={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngreHandler}/>
         <IngredientList 
         onRemoveItem={removeIngredientHandler}
         ingredients={userIngedients}/>
