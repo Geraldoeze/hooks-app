d60a4f5b68ceb49dda813aa5522fda4a641e179c
Geraldoeze <egerald344@gmail.com>
2022-01-23 22:06:04 +0100

    added fetch request

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+
+
+  const [ userIngredients, setUserIngredients] = useState([]);
+
+
+  const addIngredientHandler = ingredient => {
+
+    setUserIngredients(prevIngredients => 
+      [...prevIngredients,
+         {id: Math.random().toString(),
+        ...ingredient 
+        }
+      ]
+    )
+    fetch( `https://react-hooks-f4580-default-rtdb.firebaseio.com/ingredients.json`,{
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => { 
+      // this fetches the response from firebase that has a unique id
+      return response.json();
+    }).then(responseData => {
+      // the unique id is passed to our userIngredients
+        setUserIngredients(prevIngredients => 
+          [...prevIngredients,
+            {id: responseData.name,
+            ...ingredient 
+            }
+          ]
+        )
+    })
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList onRemoveItem={()=>{}} ingredients={userIngredients}/>
       </section>
     </div>
   );
