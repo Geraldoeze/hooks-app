04fae2b6b03169c2fd480e57bb199dd2265d983a
Geraldoeze <egerald344@gmail.com>
2022-01-23 22:19:03 +0100

    first stage

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -9,7 +9,22 @@ const Ingredients = () => {
 
   const [ userIngredients, setUserIngredients] = useState([]);
 
-
+  useEffect(()=>{
+    fetch( `https://react-hooks-f4580-default-rtdb.firebaseio.com/ingredients.json`).then(
+      response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    })
+  }, []);
+      
   const addIngredientHandler = ingredient => {
 
     setUserIngredients(prevIngredients => 
